3d89dca10c15e299e960df38de85fd5530dc4d3e
Bo Peterson <bo.peterson@gmail.com>
2020-03-24 12:44:41 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,24 @@
-    function preload ()
+var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);    
+
+function preload ()
     {
       this.load.image('ground','assets/ground.png'); //1
       this.load.image('ball','assets/ball.png'); //3
