1ade75dca5e5f17508c0322dd9184312f4d22195
Bo Peterson <bo.peterson@gmail.com>
2020-03-24 13:46:56 +0100

    Update game.js

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ var config = {
       cursors = this.input.keyboard.createCursorKeys(); //8
 
       //14
-      for (var number=0;number<8;number=number+1) { //number++
+      for (var number=0;number<7;number=number+1) { //number++
         this.add.image(40+number*80,300,'star');
       }
 
@@ -67,6 +67,6 @@ var config = {
 
 
   function destroyShip(pointer, gameObject) {
-    gameObject.setVelocityY(10);
+    gameObject.setVelocityX(100);
     //gameObject.play("explode");
   }
